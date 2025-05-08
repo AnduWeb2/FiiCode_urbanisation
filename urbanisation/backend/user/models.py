04a20ba45bf266462481fc105058b0f27f725b67
AnduWeb2 <alexandruapetrei07@gmail.com>
2025-05-08 23:38:59 +0300

    added CustomUser model

--- urbanisation/backend/user/models.py
+++ urbanisation/backend/user/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from files.models import File
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+
+
+
 
 class StaffUser(models.Model):
     username = models.CharField(max_length=150, unique=True)
